f26899c28fc400c6c3366ec973721ec374bc6040
cywambui <cywambui0@gmail.com>
2023-04-06 16:46:22 +0300

    front-end part

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$ServerName ="localhost";
+$User = "root";
+$Password="";
+$dbName = "loginsystem";
+
+$con = mysqli_connect($ServerName, $User, $Password, $dbName);
---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+   include_once'connect.php'
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>login</title>
+</head>
+<body>
+
+</body>
+</html>
